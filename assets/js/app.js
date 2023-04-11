e047e5ce7bd7b1c25dad338365b57894f769f661
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-11 15:00:37 +0200

    ADD random e-mails generation output

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,29 @@
+/*
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
 const { createApp } = Vue
   
 createApp({
   data() {
     return {
       message: 'Hello Vue!',
+      emailsToGenerate: 10,
+      generationComplete: false,
+      apiURL: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      generatedEmails: [],
+    }
+  },
+  mounted() {
+    for (let i = 0; i < this.emailsToGenerate; i++) {
+      axios
+      .get(this.apiURL)
+      .then(response => {
+        this.generatedEmails.push(response.data.response);
+        i === this.emailsToGenerate - 1 ? this.generationComplete = true : '';
+      })
     }
   }
 }).mount('#app')
